a08d25bc98fbbd4d88babf2698522f888d81c0ca
David McClure <davidwilliammcclure@gmail.com>
2021-06-22 14:15:36 -0400

    Try zustand in the arxiv demo.

--- examples/arxiv/main.tsx
+++ examples/arxiv/main.tsx
@@ -2,8 +2,9 @@
 
 import React, { useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
+import create from 'zustand';
 
-import Plot, { Bounds } from '../../src';
+import Plot from '../../src/plot';
 
 import './index.css';
 
@@ -17,33 +18,44 @@ interface Point {
 }
 
 
-function Page() {
+interface StoreState {
+  points: null | Point[];
+  setPoints: (points: Point[]) => void;
+}
+
+
+const useStore = create<StoreState>(set => ({
+  points: null,
+  setPoints: (points: Point[]) => set({points}),
+}));
+
+
+function PlotWrapper(props: { points: Point[] }) {
 
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
-    (async () => {
 
-      const points: Point[] = await fetch('/arxiv/arxiv.json.gz')
-        .then(res => res.json());
+    const plot = new Plot<Point>({
+      canvas: canvasRef.current!,
+      points: props.points,
+      getPosition: p => p.position,
+      getSize: () => 1,
+      getMaxSize: () => 100,
+      getColor: () => [72/255, 120/255, 208/255],
+      xyScale: 200,
+      shaderOpts: {
+        bigAlpha: 0.95,
+        bigEdge1: 10,
+        bigEdge2: 30,
+        maxFastSize: 10,
+      }
+    });
 
-      const plot = new Plot<Point>({
-        canvas: canvasRef.current!,
-        points,
-        getPosition: p => p.position,
-        getSize: () => 1,
-        getMaxSize: () => 200,
-        getColor: () => [72/255, 120/255, 208/255],
-        xyScale: 200,
-        shaderOpts: {
-          bigAlpha: 0.5,
-          bigEdge1: 10,
-          bigEdge2: 30,
-          maxFastSize: 10,
-        }
-      });
+    return () => {
+      plot.destroy();
+    }
 
-    })();
   })
 
   return (
@@ -51,6 +63,27 @@ function Page() {
       <canvas ref={canvasRef}></canvas>
     </div>
   )
+
+}
+
+
+function Page() {
+
+  const points = useStore(state => state.points);
+  const setPoints = useStore(state => state.setPoints);
+
+  useEffect(() => {
+    (async () => {
+
+      const points: Point[] = await fetch('/arxiv/arxiv.json.gz')
+        .then(res => res.json());
+
+      setPoints(points);
+
+    })();
+  }, [])
+
+  return points ? <PlotWrapper points={points} /> : null;
 
 }
 
